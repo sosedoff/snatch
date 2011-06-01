64df173177c2abf285249e8618335cc24eb29eee
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-06-01 18:20:29 -0500

    snatch by local aliases

--- lib/snatch.rb
+++ lib/snatch.rb
@@ -7,6 +7,10 @@ require 'snatch/handler'
 require 'snatch/session'
 
 module Snatch
+  def self.root
+    File.join(ENV['HOME'], '.snatch')
+  end
+  
   def self.run(config)
     s = Snatch::Session.new(config)
     s.run!
